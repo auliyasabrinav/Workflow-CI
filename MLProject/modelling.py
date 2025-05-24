9d633d2878e3892e983aa10318fc81a8994ad6f3
Auliya Sabrina <155187261+auliyasabrinav@users.noreply.github.com>
2025-05-24 22:57:06 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -18,22 +18,19 @@ def main(data_path):
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-with mlflow.start_run():
-    model = RandomForestClassifier(random_state=42)
-    model.fit(X_train, y_train)
+    with mlflow.start_run():
+        model = RandomForestClassifier(random_state=42)
+        model.fit(X_train, y_train)
 
-    os.makedirs("output", exist_ok=True)
-    model_path = "output/model.pkl"
-    joblib.dump(model, model_path)
+        os.makedirs("output", exist_ok=True)
+        model_path = "output/model.pkl"
+        joblib.dump(model, model_path)
 
-    # log model artifact ke MLflow
-    mlflow.log_artifact(model_path)
+        mlflow.log_artifact(model_path)
 
-    y_pred = model.predict(X_test)
-    acc = accuracy_score(y_test, y_pred)
-    print(f"Akurasi: {acc}")
-
-
+        y_pred = model.predict(X_test)
+        acc = accuracy_score(y_test, y_pred)
+        print(f"Akurasi: {acc}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
