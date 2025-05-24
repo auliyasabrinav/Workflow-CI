92f450e14e258de93ef9cc13677c011eb5436a5c
Auliya Sabrina <155187261+auliyasabrinav@users.noreply.github.com>
2025-05-24 22:54:19 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -18,7 +18,7 @@ def main(data_path):
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    with mlflow.start_run():
+with mlflow.start_run():
     model = RandomForestClassifier(random_state=42)
     model.fit(X_train, y_train)
 
@@ -26,12 +26,13 @@ def main(data_path):
     model_path = "output/model.pkl"
     joblib.dump(model, model_path)
 
-    # Log artifact ke MLflow
+    # log model artifact ke MLflow
     mlflow.log_artifact(model_path)
 
     y_pred = model.predict(X_test)
     acc = accuracy_score(y_test, y_pred)
     print(f"Akurasi: {acc}")
+
 
 
 if __name__ == "__main__":
